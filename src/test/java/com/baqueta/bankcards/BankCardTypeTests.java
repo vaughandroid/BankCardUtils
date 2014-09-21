db558fbb5a6dc7a385d5afd9fbc9907c21c3583b
Chris Vaughan <c.vaughan@outlook.com>
2014-09-21 14:11:05 +0100

    Moved test card numbers to new class. Misc test cleanup.

--- src/test/java/com/baqueta/bankcards/BankCardTypeTests.java
+++ src/test/java/com/baqueta/bankcards/BankCardTypeTests.java
@@ -11,47 +11,10 @@ import java.util.Set;
  */
 public class BankCardTypeTests extends TestCase {
 
-    static final String VALID_AMERICAN_EXPRESS = "343434343434343";
-    static final String VALID_BANKCARD = "5602 2100 0000 0006";
-    static final String VALID_CHINA_UNIONPAY = "6200 0000 0000 0000";
-    static final String VALID_DINERS_CLUB_CARTE_BLANCHE = "3000 0000 0000 04";
-    static final String VALID_DINERS_CLUB_ENROUTE = "2014 0000 0000 009";
-    static final String VALID_DINERS_CLUB_INTERNATIONAL = "3670 0102 0000 00";
-    static final String VALID_DINERS_CLUB_US_AND_CANADA = "5400 0000 0000 0005";
-    static final String VALID_DISCOVER = "6011 0004 0000 0000";
-    static final String VALID_INTERPAYMENT = "6360 0000 0000 0001";
-    static final String VALID_INSTAPAYMENT = "6370 0000 0000 0009";
-    static final String VALID_JCB = "3528 0007 0000 0000";
-    static final String VALID_LASER = "6304 9506 0000 0000 00";
-    static final String VALID_MAESTRO = "6759 6498 2643 8453";
-    static final String VALID_DANKORT = "5019 7170 1010 3742";
-    static final String VALID_MASTERCARD = "5555 5555 5555 4444";
-    static final String VALID_SOLO = "6334 5805 0000 0000";
-    static final String VALID_SWITCH = "6331 1019 9999 0016";
-    static final String VALID_VISA = "4444 3333 2222 1111";
-    static final String VALID_VISA_ELECTRON = "4917 3008 0000 0000";
-
-    private static final String[] PARTIALS_AMERICAN_EXPRESS = { "3", "34", "37" };
-    
-    private static void assertContains(Set<?> set, Object object) {
-        if (!set.contains(object)) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("\"" + object + "\" does not exist in set: {");
-            for (Object obj : set) {
-                sb.append(obj);
-            }
-            if (!set.isEmpty()) {
-                sb.delete(sb.length() - 1, sb.length());
-            }
-            sb.append('}');
-            fail(sb.toString());
-        }
-    }
-
     // TODO: Test normalised & non-normalised number strings for each method
 
     public void test_isValid_ValidAmex_ReturnsTrue() {
-        assertTrue(BankCardType.AMERICAN_EXPRESS.isValid(VALID_AMERICAN_EXPRESS));
+        assertTrue(BankCardType.AMERICAN_EXPRESS.isValid(ValidCardNumbers.AMERICAN_EXPRESS));
     }
 
     public void test_isValid_TooShortOtherwiseValidAmex_ReturnsFalse() {
@@ -89,16 +52,16 @@ public class BankCardTypeTests extends TestCase {
     }
 
     public void test_getPotentialMatches_ValidAmex_ReturnedSetIncludesAmex() {
-        assertContains(
-                BankCardType.getPotentialMatches(BankCardTypeTests.VALID_AMERICAN_EXPRESS,
-                        BankCardType.AMERICAN_EXPRESS),
-                BankCardType.AMERICAN_EXPRESS);
+        Set<BankCardType> set = BankCardType.getPotentialMatches(ValidCardNumbers.AMERICAN_EXPRESS, BankCardType.AMERICAN_EXPRESS);
+        assertTrue(set.contains(BankCardType.AMERICAN_EXPRESS));
     }
 
     public void test_getPotentialMatches_PartialAmex_ReturnedSetIncludesAmex() {
-        for (String potential : PARTIALS_AMERICAN_EXPRESS) {
-            assertTrue("Failed for \"" + potential + "\"",
-                    BankCardType.getPotentialMatches(potential, BankCardType.AMERICAN_EXPRESS)
+        // TODO: Switch to JUnit 4 & use parameterised test
+        for (int i = 1; i < ValidCardNumbers.AMERICAN_EXPRESS.length() - 1; i++) {
+            String potentialMatch = ValidCardNumbers.AMERICAN_EXPRESS.substring(0, i);
+            assertTrue("Failed for \"" + potentialMatch + "\"",
+                    BankCardType.getPotentialMatches(potentialMatch, BankCardType.AMERICAN_EXPRESS)
                             .contains(BankCardType.AMERICAN_EXPRESS));
         }
     }
@@ -109,7 +72,7 @@ public class BankCardTypeTests extends TestCase {
 
     public void test_getPotentialMatches_TooLongAmex_ReturnedSetExcludesAmex() {
         assertFalse(
-                BankCardType.getPotentialMatches(BankCardTypeTests.VALID_AMERICAN_EXPRESS + "0",
-                        BankCardType.AMERICAN_EXPRESS).contains(BankCardType.AMERICAN_EXPRESS));
+                BankCardType.getPotentialMatches(ValidCardNumbers.AMERICAN_EXPRESS + "0", BankCardType.AMERICAN_EXPRESS)
+                        .contains(BankCardType.AMERICAN_EXPRESS));
     }
 }
--- src/test/java/com/baqueta/bankcards/BankCardTypeValidityTests.java
+++ src/test/java/com/baqueta/bankcards/BankCardTypeValidityTests.java
@@ -8,78 +8,78 @@ import junit.framework.TestCase;
 public class BankCardTypeValidityTests extends TestCase {
 
     public void test_validAmex_isValid() {
-        assertTrue(BankCardType.AMERICAN_EXPRESS.isValid(BankCardTypeTests.VALID_AMERICAN_EXPRESS));
+        assertTrue(BankCardType.AMERICAN_EXPRESS.isValid(ValidCardNumbers.AMERICAN_EXPRESS));
     }
 
     public void test_validBankcard_isValid() {
-        assertTrue(BankCardType.BANKCARD.isValid(BankCardTypeTests.VALID_BANKCARD));
+        assertTrue(BankCardType.BANKCARD.isValid(ValidCardNumbers.BANKCARD));
     }
 
     public void test_validChinaUnionpay_isValid() {
-        assertTrue(BankCardType.CHINA_UNIONPAY.isValid(BankCardTypeTests.VALID_CHINA_UNIONPAY));
+        assertTrue(BankCardType.CHINA_UNIONPAY.isValid(ValidCardNumbers.CHINA_UNIONPAY));
     }
 
     public void test_validDinersClubCarteBlanche_isValid() {
-        assertTrue(BankCardType.DINERS_CLUB_CARTE_BLANCHE.isValid(BankCardTypeTests.VALID_DINERS_CLUB_CARTE_BLANCHE));
+        assertTrue(BankCardType.DINERS_CLUB_CARTE_BLANCHE.isValid(ValidCardNumbers.DINERS_CLUB_CARTE_BLANCHE));
     }
 
     public void test_validDinersClubEnroute_isValid() {
-        assertTrue(BankCardType.DINERS_CLUB_ENROUTE.isValid(BankCardTypeTests.VALID_DINERS_CLUB_ENROUTE));
+        assertTrue(BankCardType.DINERS_CLUB_ENROUTE.isValid(ValidCardNumbers.DINERS_CLUB_ENROUTE));
     }
 
     public void test_validDinersClubInternational_isValid() {
-        assertTrue(BankCardType.DINERS_CLUB_INTERNATIONAL.isValid(BankCardTypeTests.VALID_DINERS_CLUB_INTERNATIONAL));
+        assertTrue(BankCardType.DINERS_CLUB_INTERNATIONAL.isValid(ValidCardNumbers.DINERS_CLUB_INTERNATIONAL));
     }
 
     public void test_validDinersClubUSAndCanada_isValid() {
-        assertTrue(BankCardType.DINERS_CLUB_US_AND_CANADA.isValid(BankCardTypeTests.VALID_DINERS_CLUB_US_AND_CANADA));
+        assertTrue(BankCardType.DINERS_CLUB_US_AND_CANADA.isValid(ValidCardNumbers.DINERS_CLUB_US_AND_CANADA));
     }
 
     public void test_validDiscover_isValid() {
-        assertTrue(BankCardType.DISCOVER.isValid(BankCardTypeTests.VALID_DISCOVER));
+        assertTrue(BankCardType.DISCOVER.isValid(ValidCardNumbers.DISCOVER));
     }
 
     public void test_validInterpayment_isValid() {
-        assertTrue(BankCardType.INTERPAYMENT.isValid(BankCardTypeTests.VALID_INTERPAYMENT));
+        assertTrue(BankCardType.INTERPAYMENT.isValid(ValidCardNumbers.INTERPAYMENT));
     }
 
     public void test_validInstapayment_isValid() {
-        assertTrue(BankCardType.INSTAPAYMENT.isValid(BankCardTypeTests.VALID_INSTAPAYMENT));
+        assertTrue(BankCardType.INSTAPAYMENT.isValid(ValidCardNumbers.INSTAPAYMENT));
     }
 
     public void test_validJCB_isValid() {
-        assertTrue(BankCardType.JCB.isValid(BankCardTypeTests.VALID_JCB));
+        assertTrue(BankCardType.JCB.isValid(ValidCardNumbers.JCB));
     }
 
     public void test_validLaser_isValid() {
-        assertTrue(BankCardType.LASER.isValid(BankCardTypeTests.VALID_LASER));
+        assertTrue(BankCardType.LASER.isValid(ValidCardNumbers.LASER));
     }
 
     public void test_validMaestro_isValid() {
-        assertTrue(BankCardType.MAESTRO.isValid(BankCardTypeTests.VALID_MAESTRO));
+        assertTrue(BankCardType.MAESTRO.isValid(ValidCardNumbers.MAESTRO));
     }
 
     public void test_validDankort_isValid() {
-        assertTrue(BankCardType.DANKORT.isValid(BankCardTypeTests.VALID_DANKORT));
+        assertTrue(BankCardType.DANKORT.isValid(ValidCardNumbers.DANKORT));
     }
 
     public void test_validMastercard_isValid() {
-        assertTrue(BankCardType.MASTERCARD.isValid(BankCardTypeTests.VALID_MASTERCARD));
+        assertTrue(BankCardType.MASTERCARD.isValid(ValidCardNumbers.MASTERCARD));
     }
 
     public void test_validSolo_isValid() {
-        assertTrue(BankCardType.SOLO.isValid(BankCardTypeTests.VALID_SOLO));
+        assertTrue(BankCardType.SOLO.isValid(ValidCardNumbers.SOLO));
     }
 
     public void test_validSwitch_isValid() {
-        assertTrue(BankCardType.SWITCH.isValid(BankCardTypeTests.VALID_SWITCH));
+        assertTrue(BankCardType.SWITCH.isValid(ValidCardNumbers.SWITCH));
     }
 
     public void test_validVisa_isValid() {
-        assertTrue(BankCardType.VISA.isValid(BankCardTypeTests.VALID_VISA));
+        assertTrue(BankCardType.VISA.isValid(ValidCardNumbers.VISA));
     }
 
     public void test_validVisaElectron_isValid() {
-        assertTrue(BankCardType.VISA_ELECTRON.isValid(BankCardTypeTests.VALID_VISA_ELECTRON));
+        assertTrue(BankCardType.VISA_ELECTRON.isValid(ValidCardNumbers.VISA_ELECTRON));
     }
 }
